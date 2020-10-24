c4b2aae58e81a26ff43414f8c066d1ee560db5b7
Diego Prestes <diegoprestes.git@hotmail.com>
2020-10-24 14:53:45 -0300

    feat: getting the balance of the account

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,12 @@
-const Token = artifacts.require("Token");
-const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token")
+const EthSwap = artifacts.require("EthSwap")
 
-module.exports = function(deployer) {
-  deployer.deploy(Token);
-  deployer.deploy(EthSwap);
-};
+module.exports = async function(deployer) {
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed()
+
+  token.transfer(ethSwap.address, '1000000000000000000000000')
+}
