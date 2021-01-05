bdb46aa388667bc0eb2f96c369189a809aa6d46d
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-01-04 22:54:16 -0300

    feat: deploying smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,13 @@
 /* eslint-disable no-undef */
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
 module.exports = function(deployer) {
+  deployer.deploy(Token);
+  const token = await Token.deployed();
+
   deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  await token.trasfer(ethSwap.address, '1000000000000000000000000');
 };
