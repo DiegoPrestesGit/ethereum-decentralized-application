678ffa01577b313487c5030cb633d74a92a66de1
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-01-05 21:45:54 -0300

    feat: implemeting tests for token tranference

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,24 +1,36 @@
-const { assert } = require('chai');
+import { assert } from 'chai';
 
 const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
 require('chai').use(require('chai-as-promised')).should();
 
+let token;
+let ethSwap;
+
+beforeEach(async () => {
+  token = await Token.new();
+  ethSwap = await EthSwap.new();
+})
+
 contract('EthSwap', (accounts) => {
-  describe('EthSwap deploy', async () => {
+  describe('Token deploy', async () => {
     it('should have the right name', async () => {
-      const ethSwap = await EthSwap.new();
-      const ethSwapName = await ethSwap.name();
-      assert(ethSwapName, 'EthSwap Instant Exchange');
+      const tokenName = await token.name();
+      assert(tokenName, 'Truffle Token');
     })
   })
 
   describe('EthSwap deploy', async () => {
     it('should have the right name', async () => {
-      const ethSwap = await EthSwap.new();
       const ethSwapName = await ethSwap.name();
       assert(ethSwapName, 'EthSwap Instant Exchange');
+    })
+
+    it('should have tokens in it', async () => {
+      await token.transfer(ethSwap.address, '1000000000000000000000000');
+      const balance = await token.balanceOf(ethSwap.address);
+      assert.equal(balance.toString(), '1000000000000000000000000');
     })
   })
 })
